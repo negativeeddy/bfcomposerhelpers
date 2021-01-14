78ed5bc75ad24b9ba2a1bd3349d123a217bbcc23
Ben Williams <benwilli@microsoft.com>
2021-01-14 15:09:59 -0600

    code cleanup and add config

--- NegativeEddy.Bots.Composer.ServiceBus/ServiceBusClient.cs
+++ NegativeEddy.Bots.Composer.ServiceBus/ServiceBusClient.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Messaging.ServiceBus;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.Threading;
@@ -12,39 +13,37 @@ namespace NegativeEddy.Bots.Composer.ServiceBus
     public class ServiceBusClientService : IHostedService
     {
         private readonly ILogger<ServiceBusClientService> _logger;
+        private readonly string _connectionString;
+        private readonly string _topicName;
+        private readonly string _subscriptionName;
 
-        public ServiceBusClientService(ILogger<ServiceBusClientService> logger)
+        private ServiceBusClient _client;
+        private ServiceBusProcessor _processor;
+
+        public ServiceBusClientService(ILogger<ServiceBusClientService> logger, IConfiguration config)
         {
             _logger = logger;
+            _connectionString = config["serviceBus:connectionString"];
+            _topicName = config["serviceBus:topicName"];
+            _subscriptionName = config["serviceBus:subscriptionName"];
         }
-
-        ServiceBusClient client;
-        ServiceBusProcessor processor;
-
-        public string connectionString { get; set; }
-        public string topicName { get; set; }
-        public string subscriptionName { get; set; }
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            connectionString = "";
-            topicName = "bottopic";
-            subscriptionName = "S1";
-
             _logger.LogInformation("Starting");
 
-            client = new ServiceBusClient(connectionString);
+            _client = new ServiceBusClient(_connectionString);
             // create a processor that we can use to process the messages
-            processor = client.CreateProcessor(topicName, subscriptionName, new ServiceBusProcessorOptions());
+            _processor = _client.CreateProcessor(_topicName, _subscriptionName, new ServiceBusProcessorOptions());
 
             // add handler to process messages
-            processor.ProcessMessageAsync += MessageHandler;
+            _processor.ProcessMessageAsync += MessageHandler;
 
             // add handler to process any errors
-            processor.ProcessErrorAsync += ErrorHandler;
+            _processor.ProcessErrorAsync += ErrorHandler;
 
             // start processing 
-            await processor.StartProcessingAsync();
+            await _processor.StartProcessingAsync();
             _logger.LogInformation("Started");
         }
 
@@ -53,23 +52,28 @@ namespace NegativeEddy.Bots.Composer.ServiceBus
             try
             {
                 _logger.LogInformation("Stopping the receiver");
-                await processor.StopProcessingAsync();
+                await _processor?.StopProcessingAsync();
+                _processor = null;
                 _logger.LogInformation("Stopped receiving messages");
             }
             finally
             {
-                _logger.LogInformation("Stopping the service bus client");
-                await client.DisposeAsync();
-                _logger.LogInformation("Stopped the service bus client");
+                if (_client != null)
+                {
+                    _logger.LogInformation("Stopping the service bus client");
+                    await _client.DisposeAsync();
+                    _client = null;
+                    _logger.LogInformation("Stopped the service bus client");
+                }
             }
         }
 
         private async Task MessageHandler(ProcessMessageEventArgs args)
         {
             string body = args.Message.Body.ToString();
-            _logger.LogInformation($"Received: {body} from subscription: {subscriptionName}");
+            _logger.LogInformation($"Received: {body} from subscription: {_subscriptionName}");
 
-            // complete the message. messages is deleted from the queue. 
+            // complete the message. messages are deleted from the queue. 
             await args.CompleteMessageAsync(args.Message);
         }
 
